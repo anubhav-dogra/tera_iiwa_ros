3bf150b453bb47f6d72525698ccb2b769ed9eccf
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-01-11 16:21:09 -0800

    first_commit

--- src/get_plot_data.py
+++ src/get_plot_data.py
@@ -1,3 +1,14 @@
 import rospy
-import iiwa_msgs
+from iiwa_driver.msg import AdditionalOutputs
 
+def iiwa_output_callback(incoming_msg):
+    rospy.loginfo(incoming_msg.commanded_torques)
+
+def listener():
+    rospy.init_node("get_plot_data_py", anonymous=True)
+    rospy.Subscriber("/additional_outputs", AdditionalOutputs, iiwa_output_callback)
+    rospy.spin()
+
+
+if __name__== '__main__':
+    listener()
