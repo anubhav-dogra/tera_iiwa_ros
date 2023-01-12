b7f1a120aa9b53e526fc6491849d7ac177c763ef
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-01-12 18:16:46 +0000

    trial for getFK failed

--- src/get_plot_data.cpp
+++ src/get_plot_data.cpp
@@ -1,5 +1,10 @@
 #include "ros/ros.h"
 #include "iiwa_driver/AdditionalOutputs.h"
+#include "iiwa_tools/GetFK.h"
+#include "sensor_msgs/JointState.h"
+
+sensor_msgs::JointState joint_positions;
+int n;
 
 void iiwa_output_callback(iiwa_driver::AdditionalOutputs incoming_msg){
     std::cout << incoming_msg.commanded_torques << std::endl;
@@ -7,7 +12,17 @@ void iiwa_output_callback(iiwa_driver::AdditionalOutputs incoming_msg){
     //incoming_msg.commanded_positions;
 
 }
-
+void iiwa_jointstates_callback(sensor_msgs::JointState inflow_j_states){ 
+    int n = inflow_j_states.name.size();
+    joint_positions.name.resize(n);
+    joint_positions.position.resize(n);
+    for (int i = 0; i < n; ++i)
+    {
+        joint_positions.position[i] = inflow_j_states.position[i];
+        //std::cout << joint_positions.position[i] << std::endl;
+    }
+    
+} 
 int main(int argc, char **argv){
     ros::init(argc, argv, "get_plot_data");
     ros::NodeHandle nh;
@@ -17,6 +32,10 @@ int main(int argc, char **argv){
     ROS_INFO("into the get_plot Node");
     
     ros::Subscriber sub_add = nh.subscribe("/additional_outputs", 10, iiwa_output_callback);
+    ros::Subscriber sub_joint_states = nh.subscribe("/iiwa/joint_states", 10, iiwa_jointstates_callback);
+    ros::ServiceClient client = nh.serviceClient<iiwa_tools::GetFK>("/iiwa/iiwa_fk_server");
+    iiwa_tools::GetFK srv_get_fk;
+
 
     ros::waitForShutdown();
 
