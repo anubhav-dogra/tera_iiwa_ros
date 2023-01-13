fd241ff7beaa7fb620ae2b21bbccfd0ad1c9dd5e
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-01-13 16:58:20 +0000

    get_data_editing

--- src/get_plot_data.cpp
+++ src/get_plot_data.cpp
@@ -2,10 +2,20 @@
 #include "iiwa_driver/AdditionalOutputs.h"
 #include <iiwa_tools/GetFK.h>
 #include "sensor_msgs/JointState.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/Float64MultiArray.h"
 
-sensor_msgs::JointState joint_positions;
+double joint_positions[6];
 int n;
+// Helper function to set value in a MultiArray
+void set_multi_array(std_msgs::Float64MultiArray& array, size_t i, size_t j, double val)
+{
+    assert(array.layout.dim.size() == 2);
+    size_t offset = array.layout.data_offset;
 
+    array.data[offset + i * array.layout.dim[0].stride + j] = val;
+}
 void iiwa_output_callback(iiwa_driver::AdditionalOutputs incoming_msg){
     std::cout << incoming_msg.commanded_torques << std::endl;
     //incoming_msg.external_torques;
@@ -14,15 +24,15 @@ void iiwa_output_callback(iiwa_driver::AdditionalOutputs incoming_msg){
 }
 void iiwa_jointstates_callback(sensor_msgs::JointState inflow_j_states){ 
     int n = inflow_j_states.name.size();
-    joint_positions.name.resize(n);
-    joint_positions.position.resize(n);
+    //joint_positions.name.resize(n);
+    //joint_positions.position.resize(n);
     for (int i = 0; i < n; ++i)
     {
-        joint_positions.position[i] = inflow_j_states.position[i];
-        //std::cout << joint_positions.position[i] << std::endl;
-    }
+        joint_positions[i] = inflow_j_states.position[i];
+         //std::cout << joint_positions[i] << std::endl;
     
-} 
+    } 
+}
 int main(int argc, char **argv){
     ros::init(argc, argv, "get_plot_data");
     ros::NodeHandle nh;
@@ -33,9 +43,24 @@ int main(int argc, char **argv){
     
     ros::Subscriber sub_add = nh.subscribe("/additional_outputs", 10, iiwa_output_callback);
     ros::Subscriber sub_joint_states = nh.subscribe("/iiwa/joint_states", 10, iiwa_jointstates_callback);
-    ros::ServiceClient client = nh.serviceClient<iiwa_tools::GetFK>("/iiwa/iiwa_fk_server");
-    iiwa_tools::GetFK srv_get_fk;
-    std::cout << joint_positions.position[0] << std::endl;
+    ros::ServiceClient fk_client = nh.serviceClient<iiwa_tools::GetFK>("/iiwa/iiwa_fk_server");
+    fk_client.waitForExistence();
+    std_msgs::Float64MultiArray positions_joints;
+    positions_joints.layout.dim.resize(2);
+    positions_joints.layout.data_offset = 0;
+    positions_joints.layout.dim[0].size = 1;
+    positions_joints.layout.dim[0].stride = 7;
+    positions_joints.layout.dim[1].size = 7;
+    positions_joints.layout.dim[1].stride = 0;
+    positions_joints.data.resize(1*7);
+    for (int i = 0; i < n; ++i)
+    {
+        positions_joints.data[i] = joint_positions[i];
+        std::cout <<  joint_positions[i] << std::endl;
+
+    }
+    //std::cout <<  joint_positions << std::endl;
+    //std::cout <<  positions_joints << std::endl;
 
 
     ros::waitForShutdown();
