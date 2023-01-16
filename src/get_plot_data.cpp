ad4dc63391629b66d93dfc8b1e0cc8d7d1811636
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-01-16 17:22:35 +0000

    get_Fk working

--- src/get_plot_data.cpp
+++ src/get_plot_data.cpp
@@ -1,14 +1,18 @@
 #include "ros/ros.h"
 #include "iiwa_driver/AdditionalOutputs.h"
 #include <iiwa_tools/GetFK.h>
+#include <iiwa_tools/GetJacobian.h>
 #include "sensor_msgs/JointState.h"
 #include "std_msgs/MultiArrayDimension.h"
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/Float64MultiArray.h"
 
-double joint_positions[6];
+double joint_positions_[6];
+double joint_velocities_[6];
+
 int n;
 ros::ServiceClient *fk_clientPtr;
+ros::ServiceClient *J_clientPtr;
 // Helper function to set value in a MultiArray
 void set_multi_array(std_msgs::Float64MultiArray& array, size_t i, size_t j, double val)
 {
@@ -34,35 +38,54 @@ void iiwa_jointstates_callback(sensor_msgs::JointState inflow_j_states){
     //joint_positions.position.resize(n);
     for (int i = 0; i < n; ++i)
     {
-        joint_positions[i] = inflow_j_states.position[i];
+        joint_positions_[i] = inflow_j_states.position[i];
+        joint_velocities_[i] = inflow_j_states.velocity[i];
          //std::cout << joint_positions[i] << std::endl;
     
     } 
-    std_msgs::Float64MultiArray positions_joints;
-    positions_joints.layout.dim.resize(2);
-    positions_joints.layout.data_offset = 0;
-    positions_joints.layout.dim[0].size = 1;
-    positions_joints.layout.dim[0].stride = 7;
-    positions_joints.layout.dim[1].size = 7;
-    positions_joints.layout.dim[1].stride = 0;
-    positions_joints.data.resize(1*7);
+    // // required for Inputting joint_states for GetFK
+    // std_msgs::Float64MultiArray positions_joints;
+    // positions_joints.layout.dim.resize(2);
+    // positions_joints.layout.data_offset = 0;
+    // positions_joints.layout.dim[0].size = 1;
+    // positions_joints.layout.dim[0].stride = 7;
+    // positions_joints.layout.dim[1].size = 7;
+    // positions_joints.layout.dim[1].stride = 0;
+    // positions_joints.data.resize(1*7);
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     positions_joints.data[i] = joint_positions[i];     
+    // }
+    // std::cout <<  positions_joints << std::endl;
+
+    // iiwa_tools::GetFK get_fk_srv;
+    //ROS_INFO("calling the service");
+    // get_fk_srv.request.joints = positions_joints;
+    // if (fk_clientPtr->call(get_fk_srv))
+    // {
+    //    ROS_INFO("The iiwa_ee_state X: %f Y: %f, Z: %f", get_fk_srv.response.poses[0].position.x, get_fk_srv.response.poses[0].position.y, get_fk_srv.response.poses[0].position.z);
+    //    ROS_INFO("The iiwa_ee_state X.O: %f Y.O: %f, Z.O: %f, W.O: %f", get_fk_srv.response.poses[0].orientation.x, get_fk_srv.response.poses[0].orientation.y, get_fk_srv.response.poses[0].orientation.z, get_fk_srv.response.poses[0].orientation.w);
+    // }
+    // else
+    // {
+    //     ROS_ERROR("Failed to call service iiwa_fk_server");
+    // }
+    iiwa_tools::GetJacobian get_J_srv;
     for (int i = 0; i < n; ++i)
     {
-        positions_joints.data[i] = joint_positions[i];     
+            
+    get_J_srv.request.joint_angles.push_back(joint_positions_[i]);
+    get_J_srv.request.joint_velocities.push_back(joint_velocities_[i]);
     }
-    //std::cout <<  positions_joints << std::endl;
-
-    iiwa_tools::GetFK get_fk_srv;
-    //ROS_INFO("calling the service");
-    get_fk_srv.request.joints = positions_joints;
-    if (fk_clientPtr->call(get_fk_srv))
+   
+    if (J_clientPtr->call(get_J_srv))
     {
-       ROS_INFO("The iiwa_ee_state X: %f Y: %f, Z: %f", get_fk_srv.response.poses[0].position.x, get_fk_srv.response.poses[0].position.y, get_fk_srv.response.poses[0].position.z);
-       ROS_INFO("The iiwa_ee_state X.O: %f Y.O: %f, Z.O: %f, W.O: %f", get_fk_srv.response.poses[0].orientation.x, get_fk_srv.response.poses[0].orientation.y, get_fk_srv.response.poses[0].orientation.z, get_fk_srv.response.poses[0].orientation.w);
+       //ROS_INFO("Jacobian: " get_J_srv.response.jacobian.data[0]);
+       std::cout << get_J_srv.response.jacobian << std::endl;
     }
     else
     {
-        ROS_ERROR("Failed to call service add_two_ints");
+        ROS_ERROR("Failed to call service iiwa_jacobian_server");
     }
 }
 int main(int argc, char **argv){
@@ -75,6 +98,9 @@ int main(int argc, char **argv){
     ros::ServiceClient fk_client = nh.serviceClient<iiwa_tools::GetFK>("/iiwa/iiwa_fk_server");
     fk_clientPtr = &fk_client;
     fk_client.waitForExistence();
+    ros::ServiceClient J_client = nh.serviceClient<iiwa_tools::GetJacobian>("/iiwa/iiwa_jacobian_server");
+    J_clientPtr = &J_client;
+    J_client.waitForExistence();
 
     ros::Subscriber sub_add = nh.subscribe("/additional_outputs", 100, iiwa_output_callback);
     ros::Subscriber sub_joint_states = nh.subscribe("/iiwa/joint_states", 100, iiwa_jointstates_callback);
