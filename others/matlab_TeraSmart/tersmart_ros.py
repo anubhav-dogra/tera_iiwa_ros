058f5daa17438eb6224582ac6b400cb243e17f70
shrutic12 <shruti.chakraborty1211@gmail.com>
2023-04-20 17:30:36 +0100

    python terasmart

--- others/matlab_TeraSmart/tersmart_ros.py
+++ others/matlab_TeraSmart/tersmart_ros.py
@@ -12,15 +12,26 @@ return_value = sock.recv(8)
 print(return_value)
 sock.close()
 
-# def CallbackWrench(msg:Wrench):
-#     global Fz
-#     Fz = msg.force.z
+def CallbackWrench(msg:Wrench):
+    global prev_Fz, current_Fz,counter
+    current_Fz = msg.force.z
+    if counter == 0:
+        prev_Fz = current_Fz
+        counter +=1
+    if abs(current_Fz - prev_Fz)>=3:
+        print("THz is recording")
+
+    
 
 
-# if __name__=="__main__":
-#     global Fz
-#     rospy.init_node("terasmart_ros")
-#     rospy.wait_for_message("/cartesian_wrench")
-#     rospy.Subscriber("/cartesian_wrench",Wrench,callback=CallbackWrench)
 
-#     rospy.spin()
+if __name__=="__main__":
+    global prev_Fz, current_Fz, counter
+    counter = 0
+    prev_Fz = 0
+    current_Fz = 0
+    rospy.init_node("terasmart_ros")
+    rospy.wait_for_message("/cartesian_wrench")
+    rospy.Subscriber("/cartesian_wrench",Wrench,callback=CallbackWrench)
+
+    rospy.spin()
