fa886e7c6131baadc0c5667ef0eac0a525237058
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-06-16 16:40:35 +0100

    keyboard-teleop_working

--- scripts/key_teleop.py
+++ scripts/key_teleop.py
@@ -4,9 +4,9 @@ from __future__ import print_function
 
 import threading
 
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
+# import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
-
+import tf
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import TwistStamped
 from geometry_msgs.msg import PoseStamped
@@ -14,14 +14,10 @@ from geometry_msgs.msg import PoseStamped
 import sys
 from select import select
 
-if sys.platform == 'win32':
-    import msvcrt
-else:
-    import termios
-    import tty
 
+import termios
+import tty
 
-TwistMsg = Twist
 PoseMsg = PoseStamped
 
 msg = """
@@ -51,24 +47,30 @@ CTRL-C to quit
 """
 
 moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
+        # 'i':(1,0,0,0),
+        # 'o':(1,0,0,-1),
+        # 'j':(0,0,0,1),
+        # 'l':(0,0,0,-1),
+        # 'u':(1,0,0,1),
+        # ',':(-1,0,0,0),
+        # '.':(-1,0,0,1),
+        # 'm':(-1,0,0,-1),
+        # 'O':(1,-1,0,0),
+        # 'I':(1,0,0,0),
+        # 'J':(0,1,0,0),
+        # 'L':(0,-1,0,0),
+        # 'U':(1,1,0,0),
+        # '<':(-1,0,0,0),
+        # '>':(-1,-1,0,0),
+        # 'M':(-1,1,0,0),
+        # 't':(0,0,1,0),
+        # 'b':(0,0,-1,0),
+        '8': (1,0,0,0,0,0,0),
+        '2': (-1,0,0,0,0,0,0),
+        '4': (0,1,0,0,0,0,0),
+        '6': (0,-1,0,0,0,0,0),
+        '5': (0,0,1,0,0,0,0),
+        '0': (0,0,-1,0,0,0,0),
     }
 
 speedBindings={
@@ -83,14 +85,14 @@ speedBindings={
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cartesian_trajectory_controller/new_goal', PoseMsg, queue_size = 1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.xr = 0.0
-        self.yr = 0.0
-        self.zr = 0.0
-        self.wr = 0.0
+        self.publisher = rospy.Publisher('/cartesian_trajectory_generator/new_goal', PoseMsg, queue_size = 1)
+        self.x = trans_desired[0]
+        self.y = trans_desired[1]
+        self.z = trans_desired[2]
+        self.xr = rot_desired[0]
+        self.yr = rot_desired[1]
+        self.zr = rot_desired[2]
+        self.wr = rot_desired[3]
         self.condition = threading.Condition()
         self.done = False
 
@@ -129,7 +131,8 @@ class PublishThread(threading.Thread):
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0, 0, 0, 0, 0)
+        self.update(trans_desired[0],trans_desired[1],trans_desired[2],
+                    rot_desired[0],rot_desired[1],rot_desired[2],rot_desired[3])
         self.join()
 
     def run(self):
@@ -159,105 +162,113 @@ class PublishThread(threading.Thread):
             self.publisher.publish(pose_msg)
 
         # Publish stop message when thread exits.
-        pose.position.x = 0.0 
+        pose.position.x = -0.62 
         pose.position.y = 0.0
-        pose.position.z = 0.0
-        pose.orientation.x = 0.0
-        pose.orientation.y = 0.0
+        pose.position.z = 0.15
+        pose.orientation.x = 0.7
+        pose.orientation.y = 0.7
         pose.orientation.z = 0.0
         pose.orientation.w = 0.0
         self.publisher.publish(pose_msg)
 
 
 def getKey(settings, timeout):
-    if sys.platform == 'win32':
-        # getwch() returns a string on Windows
-        key = msvcrt.getwch()
+    tty.setraw(sys.stdin.fileno())
+    # sys.stdin.read() returns a string on Linux
+    rlist, _, _ = select([sys.stdin], [], [], timeout)
+    if rlist:
+        key = sys.stdin.read(1)
     else:
-        tty.setraw(sys.stdin.fileno())
-        # sys.stdin.read() returns a string on Linux
-        rlist, _, _ = select([sys.stdin], [], [], timeout)
-        if rlist:
-            key = sys.stdin.read(1)
-        else:
-            key = ''
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
 def saveTerminalSettings():
-    if sys.platform == 'win32':
-        return None
     return termios.tcgetattr(sys.stdin)
 
 def restoreTerminalSettings(old_settings):
-    if sys.platform == 'win32':
-        return
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-def vels(speed, turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+# def vels(speed, turn):
+#     return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
 if __name__=="__main__":
     settings = saveTerminalSettings()
 
     rospy.init_node('teleop_twist_keyboard')
+    # tf listener
+    listener = tf.TransformListener()
+    # speed = rospy.get_param("~speed", 0.5)
+    # turn = rospy.get_param("~turn", 1.0)
+    # speed_limit = rospy.get_param("~speed_limit", 1000)
+    # turn_limit = rospy.get_param("~turn_limit", 1000)
+    # repeat = rospy.get_param("~repeat_rate", 0.0)
+    # key_timeout = rospy.get_param("~key_timeout", 0.5)
+    # stamped = rospy.get_param("~stamped", True)
+    # _frame = rospy.get_param("~frame_id", 'world')
+    _frame = "world"
+    PoseMsg = PoseStamped
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
-    speed_limit = rospy.get_param("~speed_limit", 1000)
-    turn_limit = rospy.get_param("~turn_limit", 1000)
-    repeat = rospy.get_param("~repeat_rate", 0.0)
-    key_timeout = rospy.get_param("~key_timeout", 0.5)
-    stamped = rospy.get_param("~stamped", False)
-    _frame = rospy.get_param("~frame_id", '')
-    if stamped:
-        TwistMsg = TwistStamped
+    pub_thread = PublishThread(0.0) #repeat
 
-    pub_thread = PublishThread(repeat)
-
-    x = 0
-    y = 0
-    z = 0
-    th = 0
+    
     status = 0
 
     try:
         pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
+        # pub_thread.update(x, y, z, xr, yr, zr, wr)
 
         print(msg)
-        print(vels(speed,turn))
+        # print(vels(speed,turn))
         while(1):
-            key = getKey(settings, key_timeout)
+            good = False
+            while not good:
+                try:
+                    # from iiwa_link_0 to iiwa_link_ee
+                    (trans_desired, rot_desired) = listener.lookupTransform('/iiwa_link_0', '/tool_link_ee', rospy.Time(0))
+                    good = True
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    rospy.sleep(1.)
+            x = trans_desired[0]
+            y = trans_desired[1]
+            z = trans_desired[2]
+            xr = trans_desired[0]
+            yr = rot_desired[1]
+            zr = rot_desired[2]
+            wr = rot_desired[3]
+            key = getKey(settings, 0.5)#key_timeout
             if key in moveBindings.keys():
                 x = moveBindings[key][0]
                 y = moveBindings[key][1]
                 z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = min(speed_limit, speed * speedBindings[key][0])
-                turn = min(turn_limit, turn * speedBindings[key][1])
-                if speed == speed_limit:
-                    print("Linear speed limit reached!")
-                if turn == turn_limit:
-                    print("Angular speed limit reached!")
-                print(vels(speed,turn))
-                if (status == 14):
-                    print(msg)
-                status = (status + 1) % 15
+                # xr = moveBindings[key][3]
+            # elif key in speedBindings.keys():
+            #     speed = min(speed_limit, speed * speedBindings[key][0])
+            #     turn = min(turn_limit, turn * speedBindings[key][1])
+            #     if speed == speed_limit:
+            #         print("Linear speed limit reached!")
+            #     if turn == turn_limit:
+            #         print("Angular speed limit reached!")
+            #     print(vels(speed,turn))
+            #     if (status == 14):
+            #         print(msg)
+            #     status = (status + 1) % 15
             else:
                 # Skip updating cmd_vel if key timeout and robot already
                 # stopped.
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                if key == '' and x == -0.62 and y == 0 and z == 0.15:
                     continue
-                x = 0
+                x = -0.62
                 y = 0
-                z = 0
-                th = 0
+                z = 0.15
+                xr = 0.7
+                yr = 0.7
+                zr = 0
+                wr = 0              # th = 0
                 if (key == '\x03'):
                     break
 
-            pub_thread.update(x, y, z, th, speed, turn)
+            pub_thread.update(x, y, z, xr, yr, zr, wr)
 
     except Exception as e:
         print(e)
---
+++ scripts/teleop_keyboard.py
@@ -0,0 +1,143 @@
+import rospy
+from rospy import Time
+from tf2_ros import buffer
+import tf2_ros 
+from geometry_msgs.msg import PoseStamped, TransformStamped
+import time
+import tty, sys, termios
+from select import select
+
+
+pose_msg = PoseStamped()
+# def keyboard_input():
+msg = """
+Reading from the keyboard  and Publishing to Cartesian_trajectory_generator/new_goal!
+---------------------------
+all poses are w.r.t. to the Base frame
+
+8 : +x          4: (+y)
+2 : -x          6: (-y)
+5 : up (+z)
+0 : down (-z)
+
+q or CTRL-C to quit
+"""
+
+delta = 0.1
+delta_r = 0.05
+moveBindings = {
+        '8': (delta,0,0,0,0,0,0),
+        '2': (-delta,0,0,0,0,0,0),
+        '4': (0,delta,0,0,0,0,0),
+        '6': (0,-delta,0,0,0,0,0),
+        '5': (0,0,0.05,0,0,0,0),
+        '0': (0,0,-0.05,0,0,0,0),
+        'w': (0,0,0,0,0,0,delta_r),
+        'a': (0,0,0,delta_r,0,0,0),
+        's': (0,0,0,0,delta_r,0,0),
+        'd': (0,0,0,0,0,delta_r,0),
+        'i': (0,0,0,0,0,0,-delta_r),
+        'j': (0,0,0,-delta_r,0,0,0),
+        'k': (0,0,0,0,-delta_r,0,0),
+        'l': (0,0,0,0,0,-delta_r,0),
+  
+    }
+
+def restoreTerminalSettings(old_settings):
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+def saveTerminalSettings():
+    return termios.tcgetattr(sys.stdin)
+def getKey(settings, timeout):
+    tty.setraw(sys.stdin.fileno())
+    # sys.stdin.read() returns a string on Linux
+    rlist, _, _ = select([sys.stdin], [], [], timeout)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def update(x, y, z, xr, yr, zr, wr, tup):
+        # self.condition.acquire()
+        new_x = x   + tup[0]
+        new_y = y   + tup[1]
+        new_z = z   + tup[2]
+        new_xr = xr + tup[3]
+        new_yr = yr + tup[4]
+        new_zr = zr + tup[5]
+        new_wr = wr + tup[6]
+        # Notify publish thread that we have a new message.
+        # self.condition.notify()
+        # self.condition.release()
+        pose_msg.header.frame_id= "world"
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.pose.position.x = new_x
+        pose_msg.pose.position.y = new_y
+        pose_msg.pose.position.z = new_z
+        pose_msg.pose.orientation.x = new_xr
+        pose_msg.pose.orientation.y = new_yr
+        pose_msg.pose.orientation.z = new_zr
+        pose_msg.pose.orientation.w = new_wr
+
+        return pose_msg
+
+    
+# def wait_for_subscribers(self):
+#     i = 0
+#     while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+#         if i == 4:
+#             print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+#         rospy.sleep(0.5)
+#         i += 1
+#         i = i % 5
+#     if rospy.is_shutdown():
+#         raise Exception("Got shutdown request before subscribers connected")
+
+def teleoperator():
+    rospy.init_node('teleoperator', anonymous=True)
+    # rospy.Rate(10)
+    buffer_ = tf2_ros.Buffer()
+    listerner = tf2_ros.TransformListener(buffer_)
+    publish_ = rospy.Publisher("cartesian_trajectory_generator/new_goal", PoseStamped, queue_size=1)    
+    while not rospy.is_shutdown():      
+        try:
+            now = rospy.Time.now()
+            transformation = buffer_.lookup_transform("iiwa_link_0", "tool_link_ee", rospy.Time(0), timeout=rospy.Duration(2))
+            # print(transformation)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            # rospy.sleep(1.)
+            print("exception occured")  
+
+        _x = transformation.transform.translation.x
+        _y = transformation.transform.translation.y
+        _z = transformation.transform.translation.z
+        _xr = transformation.transform.rotation.x
+        _yr = transformation.transform.rotation.y
+        _zr = transformation.transform.rotation.z
+        _wr = transformation.transform.rotation.w
+
+
+        # print(_x,_y,_z,_xr,_yr,_zr,_wr)
+        key = getKey(settings, 1)
+        if key in moveBindings.keys():
+            new_pose = update(_x, _y, _z, _xr, _yr, _zr, _wr, moveBindings[key])
+            publish_.publish(new_pose)
+                
+        elif (key == '\x03'):
+            break
+        elif (key == 'q'):
+            break
+
+    restoreTerminalSettings(settings)    
+
+
+if __name__ == '__main__':
+    settings = saveTerminalSettings()
+
+    try:
+        teleoperator()
+    except rospy.ROSInterruptException:
+        pass
