8654d3541aca579460e9703c0bef31230a43f557
anubhav-dogra <anubhav.dogra4893@gmail.com>
2023-12-14 16:07:31 +0000

    raster_eye_in_hand

--- scripts/terasmart_ros.py
+++ scripts/terasmart_ros.py
@@ -5,17 +5,19 @@ import socket, struct
 import matplotlib.pyplot as plt
 
 
-def submit(command, type_id, n_bit):
-    sock = socket.socket()
-    sock.connect((ip_addr,port))
-    sock.sendall(command.encode('utf-8'))
-    return_value = [e[0] for e in list(struct.iter_unpack(type_id, sock.recv(n_bit)))]
-    sock.close()
-    return return_value
+# def submit(command, type_id, n_bit):
+#     sock = socket.socket()
+#     sock.connect((ip_addr,port))
+#     sock.sendall(command.encode('utf-8'))
+#     return_value = [e[0] for e in list(struct.iter_unpack(type_id, sock.recv(n_bit)))]
+#     sock.close()
+#     return return_value
 
 def CallbackWrench(msg):
     global prev_Fz, current_Fz, counter, counter_, t1, numpoints
     current_Fz = msg.wrench.force.z
+    # print("counter", counter, rospy.get_time())
+    rospy.loginfo("Hello")
     if counter_ == 0:
         prev_Fz = current_Fz
         print("prev_Fz:", prev_Fz)
@@ -33,42 +35,41 @@ def CallbackWrench(msg):
         if diff_time <= 60:
             print("THz is recording")
             print("current_Fz", current_Fz)
-            # print("numpoints", numpoints)
-            pulse_data = submit("GETLATESTPULSE", "d", 8*numpoints[0]) #array with the pulse data
-            print("size",sys.getsizeof(pulse_data))
+                # print("numpoints", numpoints)
+            # pulse_data = submit("GETLATESTPULSE", "d", 8*numpoints[0]) #array with the pulse data
+            # print("size",sys.getsizeof(pulse_data))
             print("counter",counter)
-            # plt.plot(pulse_data)
-            # plt.show()
+                # plt.plot(pulse_data)
+                # plt.show()
         else:
-            return_value = submit("STOP","?", 8)#Starting/Stopping Spectrometer if scanning
-        
+            # return_value = submit("STOP","?", 8)#Starting/Stopping Spectrometer if scanning
+            print("STOPPED")
         counter += 1
 
 
 if __name__=="__main__":
     rospy.init_node("terasmart_ros")
-    rate = rospy.Rate(4)
+    rate = rospy.Rate(0.25)
     global prev_Fz, current_Fz, counter, counter_, t1, numpoints
     counter = 0
     counter_ = 0
     prev_Fz = 0
     current_Fz = 0
-    # ip_addr = "137.205.241.135"
-    ip_addr = "10.216.120.118"
+        # ip_addr = "137.205.241.135"
+    # ip_addr = "10.216.120.118"
     port = 8001
     start_time = -340
     end_time = -290
-    return_value = submit("STOP","?", 8)#Starting/Stopping Spectrometer if scanning
-    return_value = submit("GETSTATUS","i",32)#getting the status
-    return_value = submit("SETSTARTVALUE -340", "?", 8)#setting the start of the scan
-    return_value = submit("SETENDVALUE -290", "?", 8)#setting the end of the scan
-    return_value = submit("START","?", 8)#StartingSpectrometer if scanning
-    numpoints = submit("GETNUMBEROFPOINTS", "i", 32) #number of points of the pulse
-    print("numpoints: ",  numpoints[0])
-    
-    time_axis = submit("GETTIMEAXIS", "d", 8*numpoints[0]) #array with the time data
-    # plt.plot(time_axis)
-    # plt.show()
+    # return_value = submit("STOP","?", 8)#Starting/Stopping Spectrometer if scanning
+    # return_value = submit("GETSTATUS","i",32)#getting the status
+    # return_value = submit("SETSTARTVALUE -340", "?", 8)#setting the start of the scan
+    # return_value = submit("SETENDVALUE -290", "?", 8)#setting the end of the scan
+    # return_value = submit("START","?", 8)#StartingSpectrometer if scanning
+    # numpoints = submit("GETNUMBEROFPOINTS", "i", 32) #number of points of the pulse
+    # print("numpoints: ",  numpoints[0])
+    # time_axis = submit("GETTIMEAXIS", "d", 8*numpoints[0]) #array with the time data
+        # plt.plot(time_axis)
+        # plt.show()
     print("unfinished")
 
     while not rospy.is_shutdown():
