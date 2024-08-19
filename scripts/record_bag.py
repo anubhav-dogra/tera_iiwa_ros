f2677796e92c9ced9a259543da9f954f4c8e851e
anubhav-dogra <anubhav.dogra4893@gmail.com>
2024-08-19 16:44:17 +0100

    syncing recording topics

--- scripts/record_bag.py
+++ scripts/record_bag.py
@@ -2,6 +2,7 @@ import rospy
 import rosbag
 from geometry_msgs.msg import WrenchStamped, TransformStamped
 import os, sys
+import message_filters
 
 
 class RecordBag:
@@ -9,20 +10,30 @@ class RecordBag:
         # get home directory
         home = os.path.expanduser("~")
         self.bag = rosbag.Bag(home + '/Recordings/bags/' +  filename, 'w')
-        self.ee_pose_sub = rospy.Subscriber('/tool_link_ee_pose', TransformStamped, self.ee_pose_callback, queue_size=10)
-        self.wrench_tool_sub = rospy.Subscriber('/cartesian_wrench_tool', WrenchStamped, self.wrench_tool_callback, queue_size=10)
+        
+        self.ee_pose_message_filter = message_filters.Subscriber('/tool_link_ee_pose', TransformStamped)
+        self.wrench_tool_message_filter = message_filters.Subscriber('/cartesian_wrench_tool', WrenchStamped)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.ee_pose_message_filter, self.wrench_tool_message_filter], 10, 0.1, allow_headerless=True)
+        self.ts.registerCallback(self.record_callback)
+        # self.ee_pose_sub = rospy.Subscriber('/tool_link_ee_pose', TransformStamped, self.ee_pose_callback, queue_size=10)
+        # self.wrench_tool_sub = rospy.Subscriber('/cartesian_wrench_tool', WrenchStamped, self.wrench_tool_callback, queue_size=10)
 
 
     def __del__(self):
         self.bag.close()
 
-    def ee_pose_callback(self, msg):
-        rospy.loginfo("Received ee_pose message")
-        self.bag.write('/tool_link_ee_pose', msg)
+    def record_callback(self, msg1, msg2):
+        # rospy.loginfo("Received ee_pose and wrench_tool message")
+        self.bag.write('/tool_link_ee_pose', msg1)
+        self.bag.write('/cartesian_wrench_tool', msg2)
 
-    def wrench_tool_callback(self, msg):
-        rospy.loginfo("Received wrench_tool message")
-        self.bag.write('/cartesian_wrench_tool', msg)
+    # def ee_pose_callback(self, msg):
+    #     rospy.loginfo("Received ee_pose message")
+    #     self.bag.write('/tool_link_ee_pose', msg)
+
+    # def wrench_tool_callback(self, msg):
+    #     rospy.loginfo("Received wrench_tool message")
+    #     self.bag.write('/cartesian_wrench_tool', msg)
 
     def run(self):
         rospy.spin()
